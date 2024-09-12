226e06f666c8f97f0a202db1f0096f962bb005d4
Guillem PRIEUR <133503458+guillemprieur@users.noreply.github.com>
2024-09-12 12:53:46 +0200

    Update mika_export.py
    Ajouts de commantaires

--- mika_export.py
+++ mika_export.py
@@ -1,15 +1,31 @@
 #importation des bibliothèques
 from PIL import Image,ImageFont,ImageDraw
 
-#la foncion imprimTxt prend une liste de chaines de caractères en entrée et inscrit ses chaines de caractères dans un fichier txt
 def imprimTxt(liste):
+    """
+    La fonction imprimTxt (jamais utilisée dans ce programme) sert à écrire une liste de chaines de caractères dans un fichier .txt.
+    Exemple : imprimTxt(["abc","bla"])
+    Ce fichier texte passeras à la ligne entre chaque case de la liste.
+    Résultat de l'exemple (dans le fichier file.txt) :
+    ------
+    abc
+    bla
+    ------
+    """
     fichier=open("file.txt","w")
     for i in range(len(liste)):
         fichier.write("\n"+liste[i])
     fichier.close()
 
-#la fonction export2Png écrit une liste de chaines de caractères dans un fichier .png
 def export2Png(liste,num="",nom="mika_export"):
+    """
+    Pour utiliser la fonction export2Png(), il faut mettre de 1 à 3 arguments :
+    liste : doit être une liste de chaines de caractères.
+    num : doit être, dans l'idéal, un chiffre ou un nombre (et prend par défaut la valeur "").
+    nom : doit être également une chaine de caractères (et prend par défaut la valeur "mika_export").
+    Exemple : export2Png(["abc","bla"],6,"monmika")
+    La fonction imprimeras donc dans une image nommée "monmika6.png" (nom+str(num)+".png") les caractères compris dans l'argument liste.
+    """
     font = ImageFont.truetype("FUTRFW.TTF", 20)
     img = Image.new("RGB", (len(liste[0])*20,len(liste)*20), color = "white")
     draw = ImageDraw.Draw(img)
@@ -17,8 +33,12 @@ def export2Png(liste,num="",nom="mika_export"):
         draw.text((0, i*20),liste[i],(0,0,0),font=font)
     img.save(nom+str(num)+".png")
 
-#la fonction export2Avi met toutes les images d'une liste dans un fichier .avi.
 def export2Avi(images,fpsvid=10):
+    """
+    Pour utiliser export2Avi() il faut mettre en argument une liste de chemins d'accès vers des fichiers images (.png) et, si on le souhaite, une valeur pour fpsvid.
+    Exemple : export2Avi(["1.png","2.png"],24)
+    Les images seront alors assemblés en une vidéo avec un fps de 10 (ou dans ce cas, de 24).
+    """
     import cv2
     frame = cv2.imread(images[0])
     height, width, layers = frame.shape
