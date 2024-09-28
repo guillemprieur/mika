88a3037727a6d2b64946357d3be879a85f61d85a
Guillem PRIEUR <133503458+guillemprieur@users.noreply.github.com>
2024-09-28 09:21:21 +0200

    Update main.py
    Ajout de la possibilité de transformer sa webcam en ASCII Art !

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from mika_create import *
 from mika_export import *
 
 #importation des biliothèques
-from time import time
+from time import time, sleep
 
 def mika_image():
     fichierImage=input("Quelle image voulez-vous traiter ? ")
@@ -25,7 +25,19 @@ def mika_video():
     export2Avi(liste_chemins,nb/duree)
     print("Votre video a bien ete exportee en "+str(int(time()-temps))+" secondes par le programme Mika. Toute l'equipe des roues detachables vous remercie")
 
+def mika_webcam(cap):
+    afficherTerminal(listeDeCaract(tableau2Ascii(numpy2Tableau(image2Numpy(reduireTaille(capturerWebcam(cap),15000))),[" ",".",":",";","*","=","(","{","$","&","@","#"]),-1,-1))
+
+
+
 if int(input("Pour quel type de fichier voulez-vous utiliser ce programme ? (1=Video ; 0=Photo)")):
-    mika_video()
+    if int(input("Voulez-vous convertir une vidéo 0=déjà enregistrée ou 1=en directe via votre webcam ?")):
+        import cv2
+        cap = cv2.VideoCapture(0)
+        while True:
+            mika_webcam(cap)
+            sleep(0.1)
+    else:
+        mika_video()
 else:
     mika_image()
